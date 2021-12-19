6a60bea63909aef44bd5c105a2a78172efb118ca
Splynya <92640840+Splynya@users.noreply.github.com>
2021-12-19 16:03:10 +0300

    Сделали что-бы всё игралось

---
+++ care/Form2.Designer.cs
@@ -0,0 +1,253 @@
+﻿
+namespace care
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.coin228text = new System.Windows.Forms.TextBox();
+            this.game_timer = new System.Windows.Forms.Timer(this.components);
+            this.coin2 = new System.Windows.Forms.PictureBox();
+            this.coin = new System.Windows.Forms.PictureBox();
+            this.obito = new System.Windows.Forms.PictureBox();
+            this.madara = new System.Windows.Forms.PictureBox();
+            this.car = new System.Windows.Forms.PictureBox();
+            this.coin1 = new System.Windows.Forms.PictureBox();
+            this.polosa3 = new System.Windows.Forms.PictureBox();
+            this.polosa2 = new System.Windows.Forms.PictureBox();
+            this.polosa1 = new System.Windows.Forms.PictureBox();
+            this.polosa = new System.Windows.Forms.PictureBox();
+            this.polosa4 = new System.Windows.Forms.PictureBox();
+            this.polosa5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.coin2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obito)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.madara)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.polosa3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.polosa2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.polosa1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.polosa)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.polosa4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.polosa5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // coin228text
+            // 
+            this.coin228text.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.coin228text.Location = new System.Drawing.Point(1, 12);
+            this.coin228text.Name = "coin228text";
+            this.coin228text.ReadOnly = true;
+            this.coin228text.Size = new System.Drawing.Size(80, 29);
+            this.coin228text.TabIndex = 19;
+            this.coin228text.Text = "Coin = 0";
+            // 
+            // game_timer
+            // 
+            this.game_timer.Tick += new System.EventHandler(this.game_timer_Tick);
+            // 
+            // coin2
+            // 
+            this.coin2.Image = ((System.Drawing.Image)(resources.GetObject("coin2.Image")));
+            this.coin2.Location = new System.Drawing.Point(180, 162);
+            this.coin2.Name = "coin2";
+            this.coin2.Size = new System.Drawing.Size(49, 50);
+            this.coin2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.coin2.TabIndex = 4;
+            this.coin2.TabStop = false;
+            this.coin2.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // coin
+            // 
+            this.coin.Image = ((System.Drawing.Image)(resources.GetObject("coin.Image")));
+            this.coin.Location = new System.Drawing.Point(65, 229);
+            this.coin.Name = "coin";
+            this.coin.Size = new System.Drawing.Size(53, 50);
+            this.coin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.coin.TabIndex = 13;
+            this.coin.TabStop = false;
+            // 
+            // obito
+            // 
+            this.obito.Image = ((System.Drawing.Image)(resources.GetObject("obito.Image")));
+            this.obito.Location = new System.Drawing.Point(56, 317);
+            this.obito.Name = "obito";
+            this.obito.Size = new System.Drawing.Size(80, 156);
+            this.obito.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.obito.TabIndex = 11;
+            this.obito.TabStop = false;
+            this.obito.Click += new System.EventHandler(this.obito_Click);
+            // 
+            // madara
+            // 
+            this.madara.Image = ((System.Drawing.Image)(resources.GetObject("madara.Image")));
+            this.madara.Location = new System.Drawing.Point(378, 162);
+            this.madara.Name = "madara";
+            this.madara.Size = new System.Drawing.Size(69, 165);
+            this.madara.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.madara.TabIndex = 3;
+            this.madara.TabStop = false;
+            this.madara.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // car
+            // 
+            this.car.BackColor = System.Drawing.Color.Transparent;
+            this.car.Image = global::care.Properties.Resources.naruto;
+            this.car.Location = new System.Drawing.Point(180, 331);
+            this.car.Name = "car";
+            this.car.Size = new System.Drawing.Size(142, 107);
+            this.car.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.car.TabIndex = 0;
+            this.car.TabStop = false;
+            // 
+            // coin1
+            // 
+            this.coin1.Image = ((System.Drawing.Image)(resources.GetObject("coin1.Image")));
+            this.coin1.Location = new System.Drawing.Point(167, 41);
+            this.coin1.Name = "coin1";
+            this.coin1.Size = new System.Drawing.Size(53, 50);
+            this.coin1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.coin1.TabIndex = 10;
+            this.coin1.TabStop = false;
+            this.coin1.Click += new System.EventHandler(this.coin1_Click);
+            // 
+            // polosa3
+            // 
+            this.polosa3.Image = ((System.Drawing.Image)(resources.GetObject("polosa3.Image")));
+            this.polosa3.Location = new System.Drawing.Point(249, 363);
+            this.polosa3.Name = "polosa3";
+            this.polosa3.Size = new System.Drawing.Size(19, 110);
+            this.polosa3.TabIndex = 8;
+            this.polosa3.TabStop = false;
+            // 
+            // polosa2
+            // 
+            this.polosa2.Image = ((System.Drawing.Image)(resources.GetObject("polosa2.Image")));
+            this.polosa2.Location = new System.Drawing.Point(249, 229);
+            this.polosa2.Name = "polosa2";
+            this.polosa2.Size = new System.Drawing.Size(19, 128);
+            this.polosa2.TabIndex = 7;
+            this.polosa2.TabStop = false;
+            // 
+            // polosa1
+            // 
+            this.polosa1.Image = ((System.Drawing.Image)(resources.GetObject("polosa1.Image")));
+            this.polosa1.Location = new System.Drawing.Point(249, 97);
+            this.polosa1.Name = "polosa1";
+            this.polosa1.Size = new System.Drawing.Size(19, 126);
+            this.polosa1.TabIndex = 6;
+            this.polosa1.TabStop = false;
+            this.polosa1.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // polosa
+            // 
+            this.polosa.Image = ((System.Drawing.Image)(resources.GetObject("polosa.Image")));
+            this.polosa.Location = new System.Drawing.Point(249, -59);
+            this.polosa.Name = "polosa";
+            this.polosa.Size = new System.Drawing.Size(19, 150);
+            this.polosa.TabIndex = 5;
+            this.polosa.TabStop = false;
+            this.polosa.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // polosa4
+            // 
+            this.polosa4.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.polosa4.Location = new System.Drawing.Point(12, -3);
+            this.polosa4.Name = "polosa4";
+            this.polosa4.Size = new System.Drawing.Size(15, 459);
+            this.polosa4.TabIndex = 14;
+            this.polosa4.TabStop = false;
+            // 
+            // polosa5
+            // 
+            this.polosa5.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.polosa5.Location = new System.Drawing.Point(453, -3);
+            this.polosa5.Name = "polosa5";
+            this.polosa5.Size = new System.Drawing.Size(15, 459);
+            this.polosa5.TabIndex = 15;
+            this.polosa5.TabStop = false;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(476, 450);
+            this.Controls.Add(this.coin228text);
+            this.Controls.Add(this.coin2);
+            this.Controls.Add(this.coin);
+            this.Controls.Add(this.obito);
+            this.Controls.Add(this.madara);
+            this.Controls.Add(this.car);
+            this.Controls.Add(this.coin1);
+            this.Controls.Add(this.polosa3);
+            this.Controls.Add(this.polosa2);
+            this.Controls.Add(this.polosa1);
+            this.Controls.Add(this.polosa);
+            this.Controls.Add(this.polosa4);
+            this.Controls.Add(this.polosa5);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.coin2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obito)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.madara)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.polosa3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.polosa2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.polosa1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.polosa)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.polosa4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.polosa5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox car;
+        private System.Windows.Forms.PictureBox madara;
+        private System.Windows.Forms.PictureBox coin2;
+        private System.Windows.Forms.PictureBox polosa;
+        private System.Windows.Forms.PictureBox polosa1;
+        private System.Windows.Forms.PictureBox polosa2;
+        private System.Windows.Forms.PictureBox polosa3;
+        private System.Windows.Forms.PictureBox coin1;
+        private System.Windows.Forms.PictureBox obito;
+        private System.Windows.Forms.PictureBox coin;
+        private System.Windows.Forms.PictureBox polosa4;
+        private System.Windows.Forms.PictureBox polosa5;
+        private System.Windows.Forms.TextBox coin228text;
+        private System.Windows.Forms.Timer game_timer;
+    }
+}
---
+++ care/Form2.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace care
+{
+    public partial class Form2 : Form
+    {
+        int timerSpeed;
+        int gameSpeed;
+        int carTunSpeed;
+        bool isGameOver;
+
+        PictureBox[] roadlines;
+        PictureBox[] enemys;
+        PictureBox[] coins;
+        Random random;
+        int collectedCoins;
+
+        void init_game()
+        {
+            timerSpeed = 100;
+            gameSpeed = 4;
+            carTunSpeed = 15;
+            isGameOver = false;
+
+            roadlines = new PictureBox[] { polosa, polosa1, polosa2,
+                polosa3};
+            enemys = new PictureBox[] { madara, obito, };
+            coins = new PictureBox[] {coin,coin1,coin2,};
+            random = new Random();
+            collectedCoins = 0;
+
+            gen_start_pos();
+
+
+
+            car.Image = Properties.Resources.naruto;
+
+            game_timer.Interval = 1000 / timerSpeed;
+            game_timer.Start();
+
+        }
+        void move_enemys()
+        {
+            int x;
+            for (int i = 0; i < enemys.Length; i++)
+            {
+                if (enemys[i].Top > this.Height)
+                {
+                    x = random.Next(polosa4.Right, polosa5.Left);
+                    enemys[i].Location = new Point(x, -enemys[i].Height);
+                }
+                else
+                {
+                    enemys[i].Top += gameSpeed;
+                }
+            }
+        }
+
+        void move_coins()
+        {
+            int x;
+            for(int i = 0; i < coins.Length; i++)
+            {
+                if(coins[i].Top > this.Height)
+                {
+                    x = random.Next(polosa4.Right, polosa5.Left - 
+                        coins[i].Width);
+                    coins[i].Location = new Point(x, -coins[i].Height);
+                }
+                else
+                {
+                    coins[i].Top += gameSpeed;
+                }
+            }
+        }
+
+        void move_lines()
+        {
+            for (int i = 0; i < roadlines.Length; i++)
+            {
+                if (roadlines[i].Top > this.Height)
+                {
+                    roadlines[i].Top = -roadlines[i].Height; 
+                }
+                else
+                {
+                    roadlines[i].Top += gameSpeed;
+                }
+            }
+
+        }
+
+        void gen_start_pos()
+        {
+            int startY = -obito.Height;
+            int x;
+            for (int i = 0; i < enemys.Length; i++)
+            {
+                x = random.Next(polosa4.Right, polosa5.Left);
+                enemys[i].Location = new Point(x, startY);
+                startY -= this.Height / enemys.Length;
+            }
+        }
+
+        public Form2()
+        {
+            InitializeComponent();
+            init_game();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void coin1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void obito_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void game_timer_Tick(object sender, EventArgs e)
+        {
+            move_enemys();
+            move_coins();
+            move_lines();
+        }
+    }
+}
